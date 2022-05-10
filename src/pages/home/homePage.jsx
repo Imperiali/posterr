d7549b5053905d01bd5824038c80c25a096e5d9d
Igor Imperiali <igor.imperiali@gmail.com>
2022-05-10 18:02:57 -0300

    feat: providers to homePage

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,8 +1,19 @@
 import Home from '../../components/pages/home'
+import PostsProvider from "../../components/shared/PostsProvider";
+import UsersProvider from "../../components/shared/UsersProvider";
+import {Outlet} from "react-router-dom"
+import UserProvider from "../../components/shared/UserProvider";
 
 const HomePage = () => {
     return (
-        <Home></Home>
+        <UserProvider>
+            <UsersProvider>
+                <PostsProvider>
+                    <Outlet/>
+                    <Home/>
+                </PostsProvider>
+            </UsersProvider>
+        </UserProvider>
     )
 }
 

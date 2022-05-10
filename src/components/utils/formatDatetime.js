a2ec67cfece543cb92ea9153e5fcbf3f3316b35d
Igor Imperiali <igor.imperiali@gmail.com>
2022-05-10 18:03:18 -0300

    feat: add util to default format of date

---
+++ src/components/utils/formatDatetime.js
@@ -0,0 +1,5 @@
+const defaultOptions = { year: 'numeric', month: 'long', day: 'numeric' }
+
+export const formatDatetime = (stringDate, options = defaultOptions) => {
+    return new Date(stringDate).toLocaleDateString("en-US", options)
+}

242e5dd34724db4d2c9a02280eda9096543088f6
Igor Imperiali <igor.imperiali@gmail.com>
2022-05-10 17:58:38 -0300

    feat: setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
 import HomePage from "./pages/home";
+import {Route, Routes} from "react-router-dom";
+import ProfilePage from "./pages/profile";
 
 function App() {
-  return (
-      <HomePage />
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} >
+                <Route path="/profile/:profileName" element={<ProfilePage/>} />
+            </Route>
+            <Route path="/:postsMode" element={<HomePage />} >
+                <Route path="/:postsMode/profile/:profileName" element={<ProfilePage/>} />
+            </Route>
+        </Routes>
+    )
 }
 
 export default App

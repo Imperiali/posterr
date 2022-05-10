d1b99eea90443e57cfe3193139d9e629ac3607db
Igor Imperiali <igor.imperiali@gmail.com>
2022-05-10 18:03:36 -0300

    feat: add avatar component

---
+++ src/components/shared/avatar/avatar.jsx
@@ -0,0 +1,10 @@
+const Avatar = (props) => {
+  const { url } = props
+
+  return (
+      <img className="w-10 h-10 rounded-full" src={url}
+           alt="Rounded avatar"/>
+  )
+}
+
+export default Avatar

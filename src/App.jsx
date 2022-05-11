b237f0bc1dcc0b435e15ac528b7661f0ddaa7ad6
Igor Imperiali <igor.imperiali@gmail.com>
2022-05-11 20:11:40 -0300

    feat: prettify main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import './App.css'
+import "./App.css";
 import HomePage from "./pages/home";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ProfilePage from "./pages/profile";
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<HomePage />} >
-                <Route path="/profile/:profileName" element={<ProfilePage/>} />
-            </Route>
-            <Route path="/:postsMode" element={<HomePage />} >
-                <Route path="/:postsMode/profile/:profileName" element={<ProfilePage/>} />
-            </Route>
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />}>
+        <Route path="/profile/:profileName" element={<ProfilePage />} />
+      </Route>
+      <Route path="/:postsMode" element={<HomePage />}>
+        <Route
+          path="/:postsMode/profile/:profileName"
+          element={<ProfilePage />}
+        />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
